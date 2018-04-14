f56ad0b7c049037f298b886f95544c94967f2eff
Fernando Pérez <fernaperg@gmail.com>
2018-04-14 19:28:15 +0200

    v1.0.2 - Release
    Update the most important method (spacy/parse).
    
    From now on, this method works as an ApiRest application and right now its much usefull.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Spacy-Web-Service
 
-v1.0.0 - Release
+v1.0.2 - Release
 
 Made by: Fernando Pérez <fernaperg@gmail.com>
 
@@ -8,11 +8,17 @@ Web Service that allows to perform all the Natural Language Processing (NLP) thr
 
    Allowed calls:
 	
+- /spacy/parse
+This is the most important method. It works as an ApiRest web service.
+Receives a json with a text (and if you want, a language) and return all the relevant information.
+Return:
+ A dictionary with the key "data" and value, the list of atributtes for each sentence.
+ For each sentence, the spacy atributtes: start, end, start_char, end_char, text, text_with_ws, orth_, label, label_, lemma, ent_id, ent_id_, sentiment and, a list of nodes and a list of edges.
+ For each node (word): text, text_with_ws, whitespace_, orth, orth_, head, left_edge, right_edge, i, ent_type, ent_type_, ent_iob, ent_iob_, ent_id, ent_id_, lemma, lemma_, norm, norm_, lower, shape, shape_, prefix, prefix_, suffix, suffix_, is_alpha, is_ascii, is_digit, is_lower, is_title, is_punct, is_left, is_left_punct, is_right_punct, is_space, is_bracket, is_quote, like_url, like_num, like_email, is_oov, is_stop, pos, pos_, tag, dep, dep_, lang, lang_, prob, idx, sentiment, lex_id, rank and cluster.
+ For each edge (relation): parent, child and dep (Ej: ADJ,...)
+
 - /spacy/set_language/<param>
 Change the server language to the one indicated by the parameter.
-
-- /spacy/get_span (The most important)
-Receive a json with a text (and if you want a language) and return all the relevant information with Spacy calculations for that phrase.
 
 - /spacy/parse_text
 It takes a json file with a text and return those text separated into phrases.
--- Spacy_web_service.py
+++ Spacy_web_service.py
@@ -10,28 +10,14 @@ nlp = spacy.load(lang)
 parse = None
 
 '''
-    Create a natural language processing object in the language indicated by parameter
+    This is a method that works like an Api Rest.
+    It receives a json with the text you want to parse and, optionally, with
+    a language (if you do not send the language, it will use the last one).
+    Returns a json with two lists, one of edges and one of nodes, as well as
+    all the attributes that may be useful.
 '''
-@get('/spacy/set_language/<param>')
-def set_language(param):
-    # If it is already preprocessed, it does not repeat the job
-    global lang
-    
-    if param == lang:
-        return {'data':True}
-    
-    try:
-        global nlp 
-        nlp = spacy.load(param)
-    except:
-        return {'data':False}
-    
-    lang = param;
-        
-    return {'data':True}
-
-@post('/spacy/get_span')
-def get_span():
+@post('/spacy/parse')
+def parse():
     text = request.json.get('text')
     local_lang = request.json.get('lang')
     
@@ -51,47 +37,115 @@ def get_span():
     
     sents = []
     for tree in parse.sents:
-        # Span
-        full = dict(text=[word.text for word in tree])
-        root = dict(root=[{word.text:word.head.text} for word in tree])
-        atr_start = {'start':tree.start}
-        atr_end = {'end':tree.end}
-        atr_start_char = {'start_char':tree.start_char}
-        atr_end_char = {'end_char':tree.end_char}
-        atr_text = {'full_text':tree.text}
-        atr_text_with_ws = {'full_text_with_ws':tree.text_with_ws}
-        #atr_orth = {'orth':tree.orth} # v2.0
-        atr_orth_ = {'orth_':tree.orth_}
-        atr_label = {'label':tree.label}
-        atr_label_ = {'label':tree.label_}
-        atr_lemma_ = {'lemma_':tree.lemma_}
-        atr_ent_id = {'ent_id':tree.ent_id}
-        atr_ent_id_ = {'ent_id_':tree.ent_id_}
-        atr_sentiment = {'sentiment':tree.sentiment}
-        # Doc.vocab
-        #vocab = tree.doc.vocab
-        #atr_vocab = {'vocab':[{'strings':vocab.strings.string},{'vectors_length':vocab.length}]}
+        nodes = []
+        edges = []
+        for word in tree:
+            edges.append({'parent': word.i, 'child':word.left_edge.i, 'dep':word.left_edge.dep_})
+            edges.append({'parent': word.i, 'child':word.right_edge.i, 'dep':word.right_edge.dep_})
+            
+            nodes_dict = {'text':word.text}
+            nodes_dict.update({'text_with_ws':word.text_with_ws})
+            nodes_dict.update({'whitespace_':word.whitespace_})
+            nodes_dict.update({'orth':word.orth})
+            nodes_dict.update({'orth_':word.orth_})
+            nodes_dict.update({'head':word.head.ent_id})
+            nodes_dict.update({'left_edge':word.left_edge.i})
+            nodes_dict.update({'right_edge':word.right_edge.i})
+            nodes_dict.update({'i':word.i})                         # -> This is what I'm using as id
+            nodes_dict.update({'ent_type':word.ent_type})
+            nodes_dict.update({'ent_type_':word.ent_type_})
+            nodes_dict.update({'ent_iob':word.ent_iob})
+            nodes_dict.update({'ent_iob_':word.ent_iob_})
+            nodes_dict.update({'ent_id':word.ent_id})
+            nodes_dict.update({'ent_id_':word.ent_id_})
+            nodes_dict.update({'lemma':word.lemma})
+            nodes_dict.update({'lemma_':word.lemma_})
+            nodes_dict.update({'norm':word.norm})
+            nodes_dict.update({'norm_':word.norm_})
+            nodes_dict.update({'lower':word.lower})
+            nodes_dict.update({'lower_':word.lower_})
+            nodes_dict.update({'shape':word.shape})
+            nodes_dict.update({'shape_':word.shape_})
+            nodes_dict.update({'prefix':word.prefix})
+            nodes_dict.update({'prefix_':word.prefix_})
+            nodes_dict.update({'suffix':word.suffix})
+            nodes_dict.update({'suffix_':word.suffix_})
+            nodes_dict.update({'is_alpha':word.is_alpha})
+            nodes_dict.update({'is_ascii':word.is_ascii})
+            nodes_dict.update({'is_digit':word.is_digit})
+            nodes_dict.update({'is_lower':word.is_lower})
+            #nodes_dict.update({'is_upper':word.is_upper})
+            nodes_dict.update({'is_title':word.is_title})
+            nodes_dict.update({'is_punct':word.is_punct})
+            nodes_dict.update({'is_left_punct':word.is_left_punct})
+            nodes_dict.update({'is_right_punct':word.is_right_punct})
+            nodes_dict.update({'is_space':word.is_space})
+            nodes_dict.update({'is_bracket':word.is_bracket})
+            nodes_dict.update({'is_quote':word.is_quote})
+            nodes_dict.update({'like_url':word.like_url})
+            nodes_dict.update({'like_num':word.like_num})
+            nodes_dict.update({'like_email':word.like_email})
+            nodes_dict.update({'is_oov':word.is_oov})
+            nodes_dict.update({'is_stop':word.is_stop})
+            nodes_dict.update({'pos':word.pos})
+            nodes_dict.update({'pos_':word.pos_})
+            nodes_dict.update({'tag':word.tag})
+            nodes_dict.update({'dep':word.dep})
+            nodes_dict.update({'dep_':word.dep_})
+            nodes_dict.update({'lang':word.lang})
+            nodes_dict.update({'lang_':word.lang_})
+            nodes_dict.update({'prob':word.prob})
+            nodes_dict.update({'idx':word.idx})
+            nodes_dict.update({'sentiment':word.sentiment})
+            nodes_dict.update({'lex_id':word.lex_id})
+            nodes_dict.update({'rank':word.rank})
+            nodes_dict.update({'cluster':word.cluster})            
+            nodes.append(nodes_dict)
+        
+        full = dict(nodes=nodes)
+        full.update(dict(edges=edges))
     
-        # I join all the dictionaries in one    
-        full.update(root)
-        full.update(atr_start)
-        full.update(atr_end)
-        full.update(atr_start_char)
-        full.update(atr_end_char)
-        full.update(atr_text)
-        full.update(atr_text_with_ws)
-        #full.update(atr_orth) # v2.0
-        full.update(atr_orth_)
-        full.update(atr_label)
-        full.update(atr_label_)
-        full.update(atr_lemma_)
-        full.update(atr_ent_id)
-        full.update(atr_ent_id_)
-        full.update(atr_sentiment)
-        #full.update(atr_vocab)
+        # I join all the dictionaries in one
+        full.update({'start':tree.start})
+        full.update({'end':tree.end})
+        full.update({'start_char':tree.start_char})
+        full.update({'end_char':tree.end_char})
+        full.update({'text':tree.text})
+        full.update({'text_with_ws':tree.text_with_ws})
+        #full.update({'orth':tree.orth}) # v2.0
+        full.update({'orth_':tree.orth_})
+        full.update({'label':tree.label})
+        full.update({'label':tree.label_})
+        full.update({'lemma_':tree.lemma_})
+        full.update({'ent_id':tree.ent_id})
+        full.update({'ent_id_':tree.ent_id_})
+        full.update({'sentiment':tree.sentiment})
+        #vocab = tree.doc.vocab
+        #full.update({'vocab':[{'strings':vocab.strings.string},{'vectors_length':vocab.length}]})
         sents.append(full)
     
     return dict(data=sents)
+
+'''
+    Create a natural language processing object in the language indicated by parameter
+'''
+@get('/spacy/set_language/<param>')
+def set_language(param):
+    # If it is already preprocessed, it does not repeat the job
+    global lang
+    
+    if param == lang:
+        return {'data':True}
+    
+    try:
+        global nlp 
+        nlp = spacy.load(param)
+    except:
+        return {'data':False}
+    
+    lang = param;
+        
+    return {'data':True}
 
 '''
     It takes a json file with a text and return thosw text separated into phrases.
@@ -144,7 +198,7 @@ def get_tags():
     ans = {}
     for word in list(parse.sents)[0]:
         ans.update({str(word):str(word.tag_)})
-    print(ans)
+    
     return ans if ans != None else {}
 
 @get('/spacy/get_tags')
@@ -159,4 +213,4 @@ def get_current_tags():
     Main
 '''
 if __name__ == "__main__":
-    run(host='localhost', port=8081, debug=True)
+    run(host='localhost', port=8080, debug=True, reloader=True)
